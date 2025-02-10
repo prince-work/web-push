0443b7e6a5ee4ef3453d8b252357eb5024d7f023
Prince Verma <139102260+prince-work@users.noreply.github.com>
2025-02-10 11:19:22 +0530

    Update chat360_sw.js

--- docs/chat360_sw.js
+++ docs/chat360_sw.js
@@ -1,4 +1,4 @@
-var config = {
+var chat360Config = {
   channelId: "4a23d8be-58a5-4c31-9a3b-41f338aa994f",
   key: "BD822-dawBIWmO3XHKZo2iho2k9ko9Jqx3MWFSagZQ3mi7tnzr4DFZnHr7FcngpGWkG8rbLJCjPXxcEr5U2mVnw"
 };
